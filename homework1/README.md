7649c927f4dd3d442f5805718b98e14ad36f0356
NMad <ngm7@students.calvin.edu>
2020-09-24 17:15:04 -0400

    Completed homework 1 app

---
+++ homework1/README.md
@@ -0,0 +1,8 @@
+# CS262 - Homework 1
+
+This homework exercise is based on NetNinja's 
+[Todo application](https://www.youtube.com/playlist?list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ) 
+(lessons 9-12)
+
+Hacks that I had to make:
+I had to add const in front of the state on line 5 of addTodo.js to get app to run.
---
+++ homework1/my-project/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  //added
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  //added
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  }
+
+
+  return (
+    <TouchableWithoutFeedback>
+      <View style={styles.container}>
+        <Header />
+        <View styles={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
